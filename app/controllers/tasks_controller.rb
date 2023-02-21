488df74476e8298ed1bd3cf2916ce332fa10e64a
Alexis Velluet <alexis.velluet@outlook.com>
2023-02-21 14:56:58 +0100

    poor style and bad visual but logic done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -14,22 +15,29 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
-    redirect_to root_path
+    redirect_to tasks_path
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
-    redirect_to root_path(@task)
+    redirect_to tasks_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path(@task), status: :see_other
   end
 
   private
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ('/')
-  root to: 'tasks#index'
-  get 'tasks/new', to: 'tasks#new'
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  post 'tasks', to: 'tasks#create'
-  get 'tasks/:id', to: 'tasks#show', as: :task
-  patch 'tasks/:id', to: 'tasks#update'
+  # root to: 'tasks#index'
+  # get 'tasks/new', to: 'tasks#new'
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
+  resources :tasks
 end
